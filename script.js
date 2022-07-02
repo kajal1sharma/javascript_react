85ef7148f6e772ae83c6e01ac68908c57e93ddfa
brainmentorspvtltd <kajal1231995@gmail.com>
2022-07-02 12:34:04 +0530

    2/7/22

---
+++ script.js
@@ -0,0 +1,47 @@
+// 2+3+4
+// console.log("hello world")
+// console.log(2+3+4);
+
+//variables=> a-z A-z 0-9 _
+//
+
+// let customerName=prompt("enter your name");
+// let firstDish =prompt("name of First Dish");
+// let secondDish=prompt("name of second dish");
+// let firstDishPrice=prompt("price of First Dish");
+// let secondDishPrice=prompt("price of Second dish");
+
+// let totalCost=Number(firstDishPrice)+Number(secondDishPrice);
+
+// let message =(`${customerName} has bought ${firstDish} and ${secondDish} 
+//        worth ruppees ${firstDishPrice} and ${secondDishPrice} and the total is ${totalCost}`)
+
+// console.log(message);
+       
+// alert(message);
+
+
+
+//a=> name of the first dish
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
