20c195eee374b61c6ef04ad830bf58dabad15181
brainmentorspvtltd <kajal1231995@gmail.com>
2022-07-05 12:12:30 +0530

    5/7

---
+++ Condition/index.js
@@ -0,0 +1,11 @@
+const moodHappy=prompt("are you happy (y/n)");
+
+if(moodHappy==='y'){
+    document.getElementById('happy').style.display="block"
+}
+// else if(exp){
+
+// }
+else{
+    document.getElementById('not-happy').style.display="block"
+}
--- test.js
+++ test.js
@@ -3,21 +3,95 @@ let b=5;
 console.log(a);
 console.log(a,b);*/
 
+
 // console.log(2+3+5+b);
 
 
-let a ;//variable declaration
-console.log(a);
-let b=12;
-a=90; //initialization
-a=89;
-a="red"
+// let a ;//variable declaration
+// console.log(a);
+// let b=12;
+// a=90; //initialization
+// a=89;
+// a="red"
 
-const c=1234;
-console.log(c);
-//c=4343;
-const PI=3.14;
-const NAMESCHOOL= "DPS"
+// const c=1234;
+// console.log(c);
+// //c=4343;
+// const PI=3.14;
+// const NAMESCHOOL= "DPS"
+
+// let aa;
+// console.log(aa);
+// aa=null;
+
+// console.log("result = ",null===undefined)
+
+// let a=123;
+// console.log(typeof a)
+
+// let result=true;//false
+// console.log(typeof result)
+// let str ="hello"
+// console.log(typeof str)
+
+//=== , ==
+// console.log("exp 1 ", 10 == '10')
+// console.log("exp 2 ",0 === '0')
+
+// console.log("exp 3 ", 10 != '10')
+// console.log("exp 4 ",0 !== '0')
+
+//falsy values
+// console.log(!!NaN )
+//undefined, null, '', 0,nan
+
+//nan => not a number
+// let str="123a";
+// console.log(Number(str));
+
+
+//{}=> truthy
+
+
+// let exp=2-2
+// if(exp){
+//     console.log("inside if");
+// }
+// else{
+//     console.log("else")
+// }
+
+//numeric FindOperators
+// let a=20;
+// let b=90;
+// console.log(a+b)
+// console.log("abc"+"abc")
+// console.log(a-b)
+// console.log(b/a);
+// console.log(a*b)
+// console.log(4%3)
+// console.log(2**3)
+
+// //90-((2+7)+(4+9))
+
+
+//conditional return boolean values
+// console.log(1>2);
+// console.log(2>1);
+
+// console.log(2>2);
+// console.log(2>=2);
+
+// console.log(3<4);
+// console.log(3<=2);
+
+
+
+
+
+
+
+
 
 
 
